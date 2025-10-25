547627ee627ba53d0bfbb9911f33c0048a7e9fe4
Rager <0ooomar.2002@gmail.com>
2025-10-26 00:38:38 +0300

    changed bloc provider place

--- bloc_project/lib/main.dart
+++ bloc_project/lib/main.dart
@@ -1,24 +1,7 @@
-import 'package:bloc_project/features/recipies/bloc/bloc/recipe_bloc_bloc.dart';
 import 'package:bloc_project/services/setup.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
   setup();
-  runApp(BlocProvider(create: (context) => RecipeBlocBloc(), child: MainApp()));
-}
-
-class MainApp extends StatelessWidget {
-  GoRouter router = GetIt.I<GoRouter>();
-  MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
-    );
-  }
+  runApp(const AppRoot());
 }
--- bloc_project/lib/services/setup.dart
+++ bloc_project/lib/services/setup.dart
@@ -4,40 +4,52 @@ import 'package:bloc_project/features/recipies/bloc/bloc/recipe_bloc_bloc.dart';
 import 'package:bloc_project/features/recipies/screen/recipies_screen.dart';
 import 'package:bloc_project/models/recipe_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 
 void setup() {
+  // Register singletons once
   GetIt.I.registerSingleton<RecipeBlocBloc>(RecipeBlocBloc());
+
   GetIt.I.registerSingleton<GoRouter>(
     GoRouter(
       routes: [
-        GoRoute(
-          path: '/',
-          builder: (BuildContext context, GoRouterState state) {
-            return RecipiesScreen();
-          },
-        ),
+        GoRoute(path: '/', builder: (context, state) => const RecipiesScreen()),
         GoRoute(
           path: '/details',
-          builder: (BuildContext context, GoRouterState state) {
-            return RecipeDetailsScreen();
-          },
+          builder: (context, state) => const RecipeDetailsScreen(),
         ),
         GoRoute(
           path: '/add',
-          builder: (BuildContext context, GoRouterState state) {
-            return AddRecipeScreen(
-              categories: [
-                CategoryType.breakfast,
-                CategoryType.launch,
-                CategoryType.dinner,
-                CategoryType.other,
-              ],
-            );
-          },
+          builder: (context, state) => AddRecipeScreen(
+            categories: const [
+              CategoryType.breakfast,
+              CategoryType.launch,
+              CategoryType.dinner,
+              CategoryType.other,
+            ],
+          ),
         ),
       ],
     ),
   );
+}
+
+class AppRoot extends StatelessWidget {
+  const AppRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final router = GetIt.I<GoRouter>();
+    final recipeBloc = GetIt.I<RecipeBlocBloc>();
+
+    return BlocProvider.value(
+      value: recipeBloc,
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: router,
+      ),
+    );
+  }
 }
